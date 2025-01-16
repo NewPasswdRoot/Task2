fdf95f2b86dd84bb02e210db33fef56d3d25f1e7
Vladimir Danilov <Kllpups@hotmail.com>
2025-01-16 11:05:09 +0300

    Добавьте файлы проекта.

---
+++ Task2/Task2.py
@@ -0,0 +1,26 @@
+text = input()
+a = text.count('a')
+if a > 0:
+    print ("Количество букв а = ", a)
+else:
+   print ("False")
+e = text.count('e')
+if e > 0:
+    print ("Количество букв e = ", e)
+else:
+   print ("False")
+i= text.count('i')
+if i > 0:
+    print ("Количество букв i = ", i)
+else:
+   print ("False")
+o = text.count('o')
+if o > 0:
+    print ("Количество букв o = ", o)
+else:
+   print ("False")
+u = text.count('u')
+if u > 0:
+    print ("Количество букв u = ", u)
+else:
+   print ("False")
